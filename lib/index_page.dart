083b3ff71029078245b3d98a4cbecc78bd95a2d0
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-02-12 14:20:52 +0530

    fix:index_page,login,main. feat:firebase login enabled, should login to access application

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,8 +1,10 @@
 import 'package:find_x/bottom_nav/notifications.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'bottom_nav/search.dart';
 import 'bottom_nav/home.dart';
 import 'bottom_navigation.dart';
+import 'login.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -22,21 +24,21 @@ class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: false,
         body: Stack(children: [
-      _tabs[_selectedIndex],
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: BottomNavigation(
-          selectedIndex: _selectedIndex,
-          onItemSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          labels: _label,
-        ),
-      ),
-    ]));
+          _tabs[_selectedIndex],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavigation(
+              selectedIndex: _selectedIndex,
+              onItemSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labels: _label,
+            ),
+          ),
+        ]));
   }
 }
