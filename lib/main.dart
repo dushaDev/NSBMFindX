c461f622662695ae1cc7b5e19542b30ff5b8740b
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-02-16 12:51:36 +0530

    feat:some changes

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorProfile.dark),
       themeMode: ThemeMode.system,
-      home:  _loadingPage(),
+      home:  IndexPage(),
     );
   }
   Widget _loadingPage() {
--- lib/res/font_profile.dart
+++ lib/res/font_profile.dart
@@ -1,4 +1,5 @@
 class FontProfile {
+  static const double extraSmall = 10.0;
   static const double small = 12.0;
   static const double medium = 16.0;
   static const double large = 24.0;
