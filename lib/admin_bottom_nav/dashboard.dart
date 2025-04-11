19213816b51ac699d8af1f5eaab8c9cbd9d2c06e
dushan6693 <dushanmadushankabeligala9@gmail.com>
2025-04-12 02:53:46 +0530

    feat: updates and verifications list added. on design changes >>dashboard.dart

--- lib/admin_bottom_nav/dashboard.dart
+++ lib/admin_bottom_nav/dashboard.dart
@@ -33,13 +33,14 @@ class _DashboardState extends State<Dashboard> {
             return Center(child: Text('error: ${snapshot.error}'));
           } else if (snapshot.hasData) {
             Map<String, String?> _getId = snapshot.data as Map<String, String?>;
-            _title = '${_readDate.getWishStatement()}, ${_getId['name']}!';
+            // _title = '${_readDate.getWishStatement()}, ${_getId['name']}!';
             return Scaffold(
               extendBody: true,
               appBar: AppBar(
                 title: Text(
                   '$_title',
-                  style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
                 ),
                 foregroundColor: Theme.of(context).colorScheme.onSurface,
                 actions: [
@@ -68,27 +69,38 @@ class _DashboardState extends State<Dashboard> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Expanded(
-                              child: _buildReportCard('Lost Report', Icons.report,
-                                  'assets/images/communication.png', () => LostPost())),
+                              child: _buildReportCard(
+                                  'Today Lost Reports',
+                                  '387',
+                                  '45%',
+                                  Icons.report,
+                                  'assets/images/communication.png',
+                                  () => LostPost())),
                           SizedBox(width: 5),
                           Expanded(
                               child: _buildReportCard(
-                                  'Found Report',
+                                  'Today Found Reports',
+                                  '314',
+                                  '38%',
                                   Icons.person_search,
                                   'assets/images/searching.png',
-                                      () =>
+                                  () =>
                                       FoundPost())), //There should be a FoundPost page
                         ],
                       ),
                       SizedBox(height: 10),
                       Column(children: [
                         FutureBuilder(
-                            future: _fireStoreService.getLostAndFoundItemsWithLimit(7),
+                            future: _fireStoreService
+                                .getLostAndFoundItemsWithLimit(7),
                             builder: (context, snapshot) {
-                              if (snapshot.connectionState == ConnectionState.waiting) {
-                                return Center(child: CircularProgressIndicator());
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return Center(
+                                    child: CircularProgressIndicator());
                               } else if (snapshot.hasError) {
-                                return Center(child: Text('error: ${snapshot.error}'));
+                                return Center(
+                                    child: Text('error: ${snapshot.error}'));
                               } else if (snapshot.hasData) {
                                 List? allItemsList = snapshot.data;
                                 final List<Map<String, dynamic>> finalList = [];
@@ -100,20 +112,24 @@ class _DashboardState extends State<Dashboard> {
                                   }
                                 }
 
-                                return _buildUpdatesSection('Updates', finalList);
+                                return _buildPendingVerificationSection(
+                                    'Pending Verifications', finalList);
                               } else {
                                 return Center(child: Text('No data found'));
                               }
                             }),
                         SizedBox(height: 10),
                         FutureBuilder(
-                            future:
-                            _fireStoreService.getLostAndFoundItemsById(_getId['id']!, 7),
+                            future: _fireStoreService
+                                .getLostAndFoundItemsWithLimit(7),
                             builder: (context, snapshot) {
-                              if (snapshot.connectionState == ConnectionState.waiting) {
-                                return Center(child: CircularProgressIndicator());
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return Center(
+                                    child: CircularProgressIndicator());
                               } else if (snapshot.hasError) {
-                                return Center(child: Text('error: ${snapshot.error}'));
+                                return Center(
+                                    child: Text('error: ${snapshot.error}'));
                               } else if (snapshot.hasData) {
                                 List? allItemsList = snapshot.data;
                                 final List<Map<String, dynamic>> finalList = [];
@@ -125,11 +141,13 @@ class _DashboardState extends State<Dashboard> {
                                   }
                                 }
 
-                                return _buildUpdatesSection('Your history', finalList);
+                                return _buildUpdatesSection(
+                                    'Updates', finalList);
                               } else {
                                 return Center(child: Text('No data found'));
                               }
                             }),
+
                       ]),
                       SizedBox(height: 80),
                     ],
@@ -140,12 +158,11 @@ class _DashboardState extends State<Dashboard> {
           } else {
             return Center(child: Text('No data found'));
           }
-
-        }
-    );
+        });
   }
+
   Widget _buildReportCard(
-      String title, IconData icon, String image, Widget Function() myPageHere) {
+      String title, String count,String pres, IconData icon, String image, Widget Function() myPageHere) {
     return Card(
         color: Theme.of(context).colorScheme.surfaceContainer,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
@@ -158,14 +175,6 @@ class _DashboardState extends State<Dashboard> {
           },
           child: Column(
             children: [
-              Image.asset(
-                image,
-                height: 130.0,
-                fit: BoxFit.fitHeight,
-                scale: 1,
-                alignment: Alignment.center,
-              ),
-              SizedBox(height: 5),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(title,
@@ -174,6 +183,39 @@ class _DashboardState extends State<Dashboard> {
                         fontSize: FontProfile.medium,
                         fontWeight: FontWeight.bold)),
               ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(count,
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.onSurface,
+                                fontSize: FontProfile.large,
+                                fontWeight: FontWeight.bold)),
+                        Text(pres,
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.onSurface,
+                                fontSize: FontProfile.medium,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    image,
+                    height: 80.0,
+                    fit: BoxFit.fitHeight,
+                    scale: 1,
+                    alignment: Alignment.center,
+                  ),
+                ],
+              ),
+
+
             ],
           ),
         ));
@@ -188,11 +230,21 @@ class _DashboardState extends State<Dashboard> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(title,
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onSurface,
-                    fontSize: FontProfile.medium,
-                    fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontSize: FontProfile.medium,
+                        fontWeight: FontWeight.bold)),
+              TextButton(onPressed: (){}, child:    Text('View All',
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      fontSize: FontProfile.medium,
+                      fontWeight: FontWeight.normal)), )
+              ],
+            ),
             SizedBox(height: 5),
             Container(
               height: 250,
@@ -204,7 +256,7 @@ class _DashboardState extends State<Dashboard> {
                     children: [
                       ListTile(
                         tileColor:
-                        Theme.of(context).colorScheme.surfaceContainer,
+                            Theme.of(context).colorScheme.surfaceContainer,
                         contentPadding: EdgeInsets.symmetric(
                           horizontal: 3,
                         ),
@@ -218,7 +270,7 @@ class _DashboardState extends State<Dashboard> {
                           width: 38,
                           height: 18,
                           padding:
-                          EdgeInsets.symmetric(horizontal: 1, vertical: 1),
+                              EdgeInsets.symmetric(horizontal: 1, vertical: 1),
                           decoration: BoxDecoration(
                             color: item['type']
                                 ? Theme.of(context).colorScheme.primary
@@ -228,7 +280,7 @@ class _DashboardState extends State<Dashboard> {
                           child: Text(item['type'] ? 'found' : 'lost',
                               style: TextStyle(
                                   color:
-                                  Theme.of(context).colorScheme.onPrimary)),
+                                      Theme.of(context).colorScheme.onPrimary)),
                         ),
                         title: Text(item['itemName']!,
                             style: TextStyle(fontWeight: FontWeight.normal)),
@@ -256,13 +308,98 @@ class _DashboardState extends State<Dashboard> {
       ),
     );
   }
-  Future<Map<String, String?>> _getIdName() async {
 
+  Widget _buildPendingVerificationSection(String title, List<Map<String, dynamic>> items) {
+    return Card(
+      color: Theme.of(context).colorScheme.surfaceContainer,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title,
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurface,
+                        fontSize: FontProfile.medium,
+                        fontWeight: FontWeight.bold)),
+                TextButton(onPressed: (){}, child:    Text('View All',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        fontSize: FontProfile.medium,
+                        fontWeight: FontWeight.normal)), )
+              ],
+            ),
+            SizedBox(height: 5),
+            Container(
+              height: 250,
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return Column(
+                    children: [
+                      ListTile(
+                        tileColor:
+                        Theme.of(context).colorScheme.surfaceContainer,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 3,
+                        ),
+                        horizontalTitleGap: 10,
+                        isThreeLine: false,
+                        minTileHeight: 30,
+                        minLeadingWidth: 10,
+                        minVerticalPadding: 5,
+                        leading: Container(
+                          alignment: Alignment.center,
+                          width: 45,
+                          height: 20,
+                          padding:
+                          EdgeInsets.symmetric(horizontal: 1, vertical: 1),
+                          decoration: BoxDecoration(
+                            color: item['type']
+                                ? Theme.of(context).colorScheme.primary
+                                : Theme.of(context).colorScheme.secondary,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
+                          child: Text(item['type'] ? 'student' : 'staff',
+                              style: TextStyle(
+                                  color:
+                                  Theme.of(context).colorScheme.onPrimary)),
+                        ),
+                        title: Text(item['itemName']!,
+                            style: TextStyle(fontWeight: FontWeight.normal)),
+                        trailing: Text(
+                            _readDate.getDuration(item['postedTime']),
+                            style: TextStyle(color: Colors.grey)),
+                      ),
+                      Divider(
+                        height: 0,
+                        thickness: 1,
+                        indent: 10.0,
+                        endIndent: 5.0,
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Future<Map<String, String?>> _getIdName() async {
     String? id = await _authService.getUserId();
     String? name = await _authService.getUserDisplayName();
-    return {'id':id,'name':name};
-
+    return {'id': id, 'name': name};
   }
+
   Widget _buildShimmerLoading() {
     return Shimmer.fromColors(
       baseColor: Theme.of(context).colorScheme.surface,
